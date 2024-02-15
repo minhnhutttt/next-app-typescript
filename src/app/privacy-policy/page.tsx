524d7a60cf70471fd9c2e51ad6513ad25b4e3ebd
rabi-minhnhut <nhut@rabi.co.jp>
2024-02-15 20:27:30 +0700

    a

--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -39,10 +39,10 @@ export default function PrivacyPolicy() {
             </ol>
           </TextBlock>
           <TextBlock title="Log Files">
-            fliQt follows a standard procedure of using log files. These files log visitors when they visit websites. All hosting companies do this as part of hosting services' analytics. The information collected by log files include internet protocol (IP) addresses, browser type, Internet Service Provider (ISP), date and time stamp, referring/exit pages, and possibly the number of clicks. These are not linked to any information that is personally identifiable. The purpose of the information is for analyzing trends, administering the site, tracking users' movement on the website, and gathering demographic information.
+            fliQt follows a standard procedure of using log files. These files log visitors when they visit websites. All hosting companies do this as part of hosting services&apos; analytics. The information collected by log files include internet protocol (IP) addresses, browser type, Internet Service Provider (ISP), date and time stamp, referring/exit pages, and possibly the number of clicks. These are not linked to any information that is personally identifiable. The purpose of the information is for analyzing trends, administering the site, tracking users&apos; movement on the website, and gathering demographic information.
           </TextBlock>
           <TextBlock title="Cookies and Web Beacons">
-            Like any other website, fliQt uses ‘cookies'. These cookies are used to store information including visitors' preferences, and the pages on the website that the visitor accessed or visited. The information is used to optimize the users' experience by customizing our web page content based on visitors' browser type and/or other information.
+            Like any other website, fliQt uses ‘cookies&apos;. These cookies are used to store information including visitors&apos; preferences, and the pages on the website that the visitor accessed or visited. The information is used to optimize the users&apos; experience by customizing our web page content based on visitors&apos; browser type and/or other information.
           </TextBlock>
           <TextBlock title="DoubleClick DART Cookie">
           Google is one of the third-party vendors on our site. It also uses cookies, known as DART cookies, to serve ads to our site visitors based upon their visit to www.fliqt.io and other sites on the internet. However, visitors may choose to decline the use of DART cookies by visiting the Google ad and content network Privacy Policy at the following URL – <a href="https://policies.google.com/technologies/ads" target="_blank" className="underline">https://policies.google.com/technologies/ads</a>. <br /><br />
@@ -53,18 +53,18 @@ export default function PrivacyPolicy() {
           </TextBlock>
           <TextBlock title="Advertising Partners Privacy Policies">
             You may consult this list to find the Privacy Policy for each of the advertising partners of fliQt. <br />
-            Third-party ad servers or ad networks use technologies like cookies, JavaScript, or Web Beacons that are used in their respective advertisements and links that appear on fliQt, which are sent directly to users' browsers. They automatically receive your IP address when this occurs. These technologies are used to measure the effectiveness of their advertising campaigns and/or to personalize the advertising content that you see on websites that you visit.<br /><br />
+            Third-party ad servers or ad networks use technologies like cookies, JavaScript, or Web Beacons that are used in their respective advertisements and links that appear on fliQt, which are sent directly to users&apos; browsers. They automatically receive your IP address when this occurs. These technologies are used to measure the effectiveness of their advertising campaigns and/or to personalize the advertising content that you see on websites that you visit.<br /><br />
             Note that fliQt has no access to or control over these cookies that are used by third-party advertisers.
           </TextBlock>
           <TextBlock title="Third-Party Privacy Policies">
-            fliQt's Privacy Policy does not apply to other advertisers or websites. Thus, we are advising you to consult the respective Privacy Policies of these third-party ad servers for more detailed information. It may include their practices and instructions about how to opt-out of certain options. You may find a complete list of these Privacy Policies and their links here: Privacy Policy Links. <br /><br />
-            You can choose to disable cookies through your individual browser options. To know more detailed information about cookie management with specific web browsers, it can be found at the browsers' respective websites. What Are Cookies?
+            fliQt&apos;s Privacy Policy does not apply to other advertisers or websites. Thus, we are advising you to consult the respective Privacy Policies of these third-party ad servers for more detailed information. It may include their practices and instructions about how to opt-out of certain options. You may find a complete list of these Privacy Policies and their links here: Privacy Policy Links. <br /><br />
+            You can choose to disable cookies through your individual browser options. To know more detailed information about cookie management with specific web browsers, it can be found at the browsers&apos; respective websites. What Are Cookies?
           </TextBlock>
           <TextBlock title="CCPA Privacy Policy (Do Not Sell My Personal Information)">
             Under the CCPA, among other rights, California consumers have the right to: <br />
-            Request that a business that collects a consumer's personal data disclose the categories and specific pieces of personal data that a business has collected about consumers.<br />
+            Request that a business that collects a consumer&apos;s personal data disclose the categories and specific pieces of personal data that a business has collected about consumers.<br />
             Request that a business delete any personal data about the consumer that a business has collected.<br />
-            Request that a business that sells a consumer's personal data, not sell the consumer's personal data.<br />
+            Request that a business that sells a consumer&apos;s personal data, not sell the consumer&apos;s personal data.<br />
             If you make a request, we have one month to respond to you. If you would like to exercise any of these rights, please contact us.
           </TextBlock>
           <TextBlock title="GDPR Privacy Policy (Data Protection Rights)">
