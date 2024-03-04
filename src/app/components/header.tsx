d97a663b68bbaaa1cf613b6634d9b2487043dc99
rabi-minhnhut <nhut@rabi.co.jp>
2024-03-04 19:28:26 +0700

    a

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,7 +1,11 @@
+"use client";
+import { useEffect, useState } from "react";
+
 export default function Header() {
+  const [open, setOpen] = useState<boolean>(false);
   return (
     <header className="absolute top-0 left-0 right-0 z-10 px-5 md:px-[60px]">
-      <div className="mx-auto max-md:py-6 w-full flex md:items-center justify-start md:h-[106px] md:gap-10 gap-5">
+      <div className="mx-auto max-md:py-6 w-full flex md:items-center justify-between md:justify-start md:h-[106px] md:gap-10 gap-5">
         <a
           href="/"
           className="duration-150 hover:opacity-75 max-md:flex-[0_0_130px]"
@@ -12,7 +16,24 @@ export default function Header() {
             alt="RWA Tag"
           />
         </a>
-        <div className="flex items-center justify-end gap-3 md:gap-5 max-md:flex-wrap">
+        <div className="flex h-[28px] w-[50px] items-center justify-center md:w-[70px] md:hidden">
+          <button
+            className={`group relative block h-[22px] w-8 cursor-pointer border-[none] p-0 max-md:z-[99] md:w-10 max-md:[&.active]:absolute ${
+              open ? "active" : ""
+            }`}
+            onClick={() => setOpen(!open)}
+          >
+            <span className="absolute left-0 top-0 block h-0.5 w-full bg-black transition-transform duration-[0.3s] ease-[ease] group-[.active]:top-[42%] group-[.active]:rotate-45"></span>
+            <span className="absolute left-0 top-2/4 block h-0.5 w-full -translate-y-2/4 bg-black transition-transform duration-[0.3s] ease-[ease] group-[.active]:opacity-0"></span>
+            <span className="absolute bottom-0 left-0 block h-0.5 w-full bg-black transition-transform duration-[0.3s] ease-[ease] group-[.active]:bottom-2/4 group-[.active]:-rotate-45"></span>
+          </button>
+        </div>
+        <div
+            className={`z-[99] flex items-end justify-center gap-6 duration-300 max-md:pointer-events-none max-md:absolute max-md:top-full max-md:left-0 max-md:right-0 max-md:flex-col max-md:opacity-0 ${
+              open ? "max-md:pointer-events-auto max-md:opacity-100" : ""
+            }`}
+          >
+        <div className="flex items-center justify-end gap-3 max-md:px-6 md:gap-5 max-md:flex-wrap">
           <a
             href="https://apps.apple.com/jp/app/diver-tag/id6449664504"
             target="_blank"
@@ -37,6 +58,7 @@ export default function Header() {
               alt="Google Play"
             />
           </a>
+        </div>
         </div>
       </div>
     </header>
