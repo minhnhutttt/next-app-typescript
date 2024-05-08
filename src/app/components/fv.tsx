2081a61341a0a464b0e8676dd8e8259870a8ddb1
rabi-minhnhut <nhut@rabi.co.jp>
2024-05-08 16:15:50 +0700

    a

---
+++ src/app/components/fv.tsx
@@ -0,0 +1,20 @@
+export default function FV() {
+    return (
+      <section className="bg-[url('/assets/images/fv-bg.png')] bg-bottom bg-[length:100%_100%] md:pt-[70px] md:pb-[310px] px-5">
+        <div className="w-full max-w-[1556px] mx-auto relative">
+            <h3 className="absolute right-[18.2%] top-0">
+                <img src="/assets/images/fv-label.png" alt="" />
+            </h3>
+            <div className="md:pt-[4.2%] md:pb-[5%]">
+                <figure>
+                    <img src="/assets/images/fv-main.png" alt="" />
+                </figure>
+            </div>
+            <h4 className="absolute left-[25.4%] bottom-0">
+                <img src="/assets/images/fv-text.png" alt="" />
+            </h4>
+        </div>
+      </section>
+    );
+  }
+  
