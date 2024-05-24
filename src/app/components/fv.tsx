613878ee5ac78393b8e904da7111b15ff9c494da
rabi-minhnhut <nhut@rabi.co.jp>
2024-05-24 17:51:13 +0700

    a

--- src/app/components/fv.tsx
+++ src/app/components/fv.tsx
@@ -8,7 +8,7 @@ const FV = () => {
   useEffect(() => {
     const images = gsap.utils.toArray<HTMLElement>(".item");
     const imageSize = images.length;
-    const rotations = [0, 18, 36, -36, -18]
+    const rotations = [0, 23, 46, -46, -23]
     const total = images.length;
     const degree = 360 / total;
 
@@ -41,7 +41,6 @@ const FV = () => {
           0
         );
 
-        let rotationAngle = index * degree;
         timeline.to(
           image,
           {
@@ -67,14 +66,14 @@ const FV = () => {
     return () => ctx.revert();
   }, []);
   return (
-    <section className="relative">
+    <section className="relative pt-5">
       <span className="absolute top-0 bg-[#0D0A8E] w-full h-[410px]"></span>
       <div className="w-full max-w-[1380px] mx-auto bg-white [box-shadow:0px_4px_60px_0px_rgba(0,_0,_0,_0.10)] rounded-[30px] relative z-10 h-[560px]">
         <div className="absolute inset-0 flex items-center justify-center">
           <img src="/assets/images/robo.png" alt="" />
         </div>
       </div>
-      <div className="w-[100vw] h-[80vh] absolute left-0 top-0 z-10 select-none overflow-hidden">
+      <div className="w-[100vw] h-[100vh] absolute left-0 top-0 z-10 select-none overflow-hidden">
         <div className="center">
           <div className="items">
             <div className="item">
