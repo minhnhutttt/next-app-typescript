1d8208f3bd7ac1690b5bf8411430cece9c74785f
rabi-minhnhut <nhut@rabi.co.jp>
2024-07-04 22:41:23 +0700

    a

--- src/components/pageTitle.tsx
+++ src/components/pageTitle.tsx
@@ -5,7 +5,7 @@ export type PageTitlePropsType = {
 };
 export default function PageTitle({ children }: PageTitlePropsType) {
   return (
-    <div className="pt-[160px] flex items-center justify-center px-5 md:pt-[200px] text-black">
+    <div className="pt-[60px] flex items-center justify-center px-5 md:pt-[120px] text-black">
       <p className="fade-up font-zen text-[7vw] font-bold md:text-[50px]">
         {children}
       </p>
