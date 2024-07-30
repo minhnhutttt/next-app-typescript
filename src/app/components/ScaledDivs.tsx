6a3389ffbcfc5df0cf21841a6136e72bb02fed42
rabi-minhnhut <nhut@rabi.co.jp>
2024-07-30 11:08:04 +0700

    a

--- src/app/components/ScaledDivs.tsx
+++ src/app/components/ScaledDivs.tsx
@@ -1,6 +1,5 @@
 "use client"
-"use client";
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 const useMousePositionPercentage = () => {
   const [position, setPosition] = useState({ xPercent: 0, yPercent: 0 });
@@ -24,21 +23,33 @@ const useMousePositionPercentage = () => {
 
 const ScaledDivs = () => {
   const { yPercent } = useMousePositionPercentage();
+  const containerRef = useRef<HTMLDivElement>(null);
+  const divRef = useRef<HTMLDivElement>(null);
 
   const topScaleY = 1.5 - ((yPercent / 100) * 1);
   const bottomScaleY = 0.5 + ((yPercent / 100) * 1);
 
+  
+  const containerHeight = containerRef.current?.clientHeight ?? 0;
+  const divHeight = divRef.current?.clientHeight ?? 0;
+
+  const bottomPx = (containerHeight - (divHeight * bottomScaleY));
 
   return (
-    <div className="">
+    <div ref={containerRef} className="relative h-screen overflow-hidden">
       <div
-        className="variable-word w-full h-[50vh]"
-        style={{ transform: `scaleY(${topScaleY})` }}
-      ></div>
+        className="variable-word w-full h-[50vh] origin-top-left"
+        style={{ transform: `translate3d(0px, 0px, 0px) scaleY(${topScaleY})` }}
+      >
+
+      </div>
       <div
-        className="variable-word w-full h-[50vh]"
-        style={{ transform: `scaleY(${bottomScaleY})` }}
-      ></div>
+        ref={divRef}
+        className="absolute inset-x-0 top-0 variable-word w-full h-[50vh] origin-top-left"
+        style={{ transform: `translate3d(0px, ${bottomPx}px, 0px) scaleY(${bottomScaleY})` }}
+      >
+        
+      </div>
     </div>
   );
 };
