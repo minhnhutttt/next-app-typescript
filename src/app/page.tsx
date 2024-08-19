4d2f79ec49808bd8dece432db3006b2c05816fc4
rabi-minhnhut <nhut@rabi.co.jp>
2024-08-19 18:25:14 +0700

    a

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,17 +4,23 @@ import { Canvas, useFrame } from "@react-three/fiber";
 import { useLoader } from "@react-three/fiber";
 import { Environment } from "@react-three/drei";
 import { DDSLoader, GLTFLoader } from "three-stdlib";
-import { Suspense, useRef, useState } from "react";
-import { Group, Mesh } from "three";
+import { Suspense, useRef, useState, useEffect } from "react";
+import { Group } from "three";
 
 interface SceneProps {
   onModelZoomed: () => void;
+  onMouseMove: () => void;
+  onModelClick: () => void;
 }
 
 THREE.DefaultLoadingManager.addHandler(/\.dds$/i, new DDSLoader());
 
-const Scene: React.FC<SceneProps> = ({ onModelZoomed }) => {
-  const gltf = useLoader(GLTFLoader, "/assets/model.gltf");
+const Scene: React.FC<SceneProps> = ({
+  onModelZoomed,
+  onMouseMove,
+  onModelClick,
+}) => {
+  const gltf = useLoader(GLTFLoader, "/assets/model/model.gltf");
   const modelRef = useRef<Group>(null);
   const [clicked, setClicked] = useState<boolean>(false);
   const [scale, setScale] = useState<number>(1);
@@ -22,10 +28,12 @@ const Scene: React.FC<SceneProps> = ({ onModelZoomed }) => {
   useFrame((state) => {
     const { mouse, viewport } = state;
     if (modelRef.current) {
+
       if (!clicked) {
         const x = mouse.x * viewport.width * 30;
         const y = mouse.y * viewport.height * 50;
         modelRef.current.lookAt(x, y, 500);
+        onMouseMove();
       } else {
         if (scale < 10) {
           setScale((prevScale) => prevScale + 0.1);
@@ -40,6 +48,7 @@ const Scene: React.FC<SceneProps> = ({ onModelZoomed }) => {
 
   const handleClick = () => {
     setClicked(true);
+    onModelClick();
   };
 
   return (
@@ -56,11 +65,41 @@ const Scene: React.FC<SceneProps> = ({ onModelZoomed }) => {
   );
 };
 
-const App: React.FC = () => {
+export default function Home() {
   const [modelZoomed, setModelZoomed] = useState<boolean>(false);
+  const audio1Ref = useRef<HTMLAudioElement>(null);
+  const audio2Ref = useRef<HTMLAudioElement>(null);
+  const [isMuted, setIsMuted] = useState(true);
+
+  useEffect(() => {
+    if (audio1Ref.current && audio2Ref.current) {
+      audio1Ref.current.muted = isMuted;
+      audio2Ref.current.muted = isMuted;
+    }
+  }, [isMuted]);
 
   const handleModelZoomed = () => {
     setModelZoomed(true);
+    if (audio1Ref.current) {
+      audio1Ref.current.pause();
+    }
+  };
+
+  const playAudio1 = () => {
+    if (audio1Ref.current) {
+      audio1Ref.current.play();
+    }
+  };
+
+  const playAudio2 = () => {
+    if (audio2Ref.current) {
+      audio2Ref.current.currentTime = 0;
+      audio2Ref.current.play();
+    }
+  };
+
+  const toggleMute = () => {
+    setIsMuted(prevState => !prevState);
   };
 
   return (
@@ -71,16 +110,32 @@ const App: React.FC = () => {
             <ambientLight intensity={0.5} />
             <directionalLight position={[10, 10, 10]} intensity={1} />
             <directionalLight position={[-10, -10, -10]} intensity={0.5} />
-            <Scene onModelZoomed={handleModelZoomed} />
+            <Scene
+              onModelZoomed={handleModelZoomed}
+              onMouseMove={playAudio1}
+              onModelClick={playAudio2}
+            />
             <Environment preset="city" />
           </Suspense>
         </Canvas>
       </div>
-      <div className={`flex items-center justify-center h-screen text-[100px] font-bold transition-opacity duration-500 ${modelZoomed ? "opacity-100" : "opacity-0 pointer-events-none"}`}>
+      <div
+        className={`flex items-center justify-center h-screen text-[100px] font-bold transition-opacity duration-500 ${
+          modelZoomed ? "opacity-100" : "opacity-0 pointer-events-none"
+        }`}
+      >
         Content
       </div>
+      <audio ref={audio1Ref} id="audio-1" src="/assets/music/bike-engine.mp3" loop></audio>
+      <audio ref={audio2Ref} id="audio-2" src="/assets/music/bike-engine.mp3"></audio>
+
+      <button onClick={toggleMute} className="fixed right-5 bottom-5 flex items-center justify-end md:h-20 h-[55px]">
+          {isMuted ?
+            <img className="md:w-[220px] w-[150px]" src="/assets/images/mute.svg" alt="" />
+            :
+            <img className="md:w-[220px] w-[150px]" src="/assets/images/unmute.svg" alt="" />
+          }
+      </button>
     </div>
   );
-};
-
-export default App;
+}
