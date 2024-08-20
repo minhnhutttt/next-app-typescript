cd3d5554968f2a1f7d6718ba8203e2435a762d36
rabi-minhnhut <nhut@rabi.co.jp>
2024-08-20 22:32:27 +0700

    a

--- src/app/components/list.tsx
+++ src/app/components/list.tsx
@@ -28,29 +28,30 @@ const List = () => {
     const ref = useScrollAnimations();
     return (
         <section ref={ref} className="relative overflow-hidden py-[100px] md:py-[160px] px-4">
-            <div className="w-full max-w-[1180px] mx-auto">
+            <div className="w-full md:max-w-[1180px] max-w-[480px] mx-auto md:space-y-20 space-y-10">
                 {data.map((item, index) => (
                     <div className="" key={index}>
-                        <div className="md:text-[40px] text-[24px] text-white font-extrabold flex items-center justify-center gap-4 h-20 md:h-[120px] relative">
+                        <div className="md:text-[40px] text-[24px] text-white font-extrabold flex items-center justify-center gap-4 h-20 md:h-[120px] relative mb-2">
                             <span>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="21" height="34" viewBox="0 0 21 34" fill="none">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="md:w-[21px] w-4" viewBox="0 0 21 34" fill="none">
                                     <path d="M20.1271 16.2441C20.5876 16.6428 20.5876 17.3572 20.1271 17.7559L1.65465 33.7536C1.00701 34.3144 -1.65467e-06 33.8544 -1.61722e-06 32.9976L-2.1866e-07 1.00237C-1.81211e-07 0.145615 1.00701 -0.314438 1.65465 0.246438L20.1271 16.2441Z" fill="white"/>
                                 </svg>
                             </span>
                             {item.topic}
                             <span className="absolute">
-                                <img src="/assets/images/deco.png" alt="" />
+                                <img className="max-md:w-[70px]" src="/assets/images/deco.png" alt="" />
                             </span>
                         </div>
-                        <div className="bg-black border-[5px] border-white rounded-[40px] md:py-[74px] py-8 md:px-10 px-5 flex items-center md:gap-[52px] gap-10">
-                            <figure>
+                        <div className="bg-black border-[5px] border-white rounded-[40px] md:py-[74px] py-8 md:px-10 px-5 flex items-center max-md:flex-col md:gap-[52px] gap-6">
+                            <figure className="max-md:w-full max-lg:w-1/2">
                                 <img src={item.image} alt="" />
                             </figure>
-                            <div className="">
-                                <div className="flex">
-                                    <h5 className="relative aspect-[351/79] bg-[url('/assets/images/bg-list.png')] bg-cover">
-                                    </h5>
-                                </div>
+                            <div className="flex-1">
+                                <h5 className="md:mb-5 mb-3 md:text-[24px] leading-none text-[18px] font-extrabold relative flex items-center justify-center w-full aspect-[351/79] md:max-w-[351px] max-w-[300px] bg-[url('/assets/images/bg-list.png')] bg-cover">
+                                    {item.label}
+                                </h5>
+                                {item.title && <p className="text-white font-extrabold md:text-[24px] text-[18px]">{item.title}</p>}
+                                <p className="md:text-[18px] text-[15px] text-white leading-[2]">{item.content}</p>
                             </div>
                         </div>
                     </div>
