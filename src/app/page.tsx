3865219a2c08d18441dc12598f22a2c02dc872ba
rabi-minhnhut <nhut@rabi.co.jp>
2024-08-21 14:24:06 +0700

    a

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -194,22 +194,31 @@ export default function Home() {
     {!modelZoomed &&
       <button
         onClick={toggleMute}
-        className="fixed right-5 bottom-5 flex items-center justify-end md:h-20 h-[55px] gap-10"
+        className="fixed right-5 bottom-5 flex items-center justify-end md:h-20 h-[40px] md:gap-10 gap-4 max-md:text-[13px]"
       >
         {isMuted ? 'Trun On' : 'Trun Off'}
-        <figure className="w-[170px] flex items-center">
+        <figure className="md:w-[170px] w-[60px] flex items-center">
         {isMuted ? (
           <img
-            className="md:w-[90px] w-[150px]"
+            className="md:w-[60px] w-[150px]"
             src="/assets/images/mute.svg"
             alt=""
           />
         ) : (
-          <img
-            className="md:w-[170px] w-[150px]"
-            src="/assets/images/unmute.svg"
-            alt=""
-          />
+          <svg className="md:w-[170px] w-[150px] animate" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 171 91" fill="none">
+          <path d="M3 38V53" stroke="black" stroke-width="6" stroke-linecap="round"/>
+          <path d="M168 37V52" stroke="black" stroke-width="6" stroke-linecap="round"/>
+          <path d="M18 24V66" stroke="black" stroke-width="6" stroke-linecap="round"/>
+          <path d="M78 24V66" stroke="black" stroke-width="6" stroke-linecap="round"/>
+          <path d="M153 24V66" stroke="black" stroke-width="6" stroke-linecap="round"/>
+          <path d="M48 29V62" stroke="black" stroke-width="6" stroke-linecap="round"/>
+          <path d="M63 35V56" stroke="black" stroke-width="6" stroke-linecap="round"/>
+          <path d="M33 15V76" stroke="black" stroke-width="6" stroke-linecap="round"/>
+          <path d="M123 21V69" stroke="black" stroke-width="6" stroke-linecap="round"/>
+          <path d="M93 13V78" stroke="black" stroke-width="6" stroke-linecap="round"/>
+          <path d="M138 9V81" stroke="black" stroke-width="6" stroke-linecap="round"/>
+          <path d="M108 3V88" stroke="black" stroke-width="6" stroke-linecap="round"/>
+        </svg>
         )}
         </figure>
       </button>
