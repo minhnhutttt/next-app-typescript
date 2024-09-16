9c2f373b710e8ce7838d8e82572de21b1e214b38
rabi-minhnhut <nhut@rabi.co.jp>
2024-09-16 17:45:52 +0700

    a

--- src/app/components/fv.tsx
+++ src/app/components/fv.tsx
@@ -9,22 +9,24 @@ const FV = () => {
                 Your browser does not support the video tag.
             </video>
         </div>
-        <div className="px-5 md:px-20 lg:px-[240px] w-full max-md:py-10 relative">
-          <div className="w-full max-md:max-w-[350px] md:w-[520px] md:space-y-6 space-y-4 max-md:mx-auto">
-            <h1 className="u-text-gradient bg-[linear-gradient(90deg,_#D9748F_0%,#0059BA_100%)] text-[30px] md:text-[48px] font-bold leading-[1.2] md:leading-none">
-            Join the <br className="max-md:hidden" />
-            Future of <br className="md:hidden" />Medicine <br className="max-md:hidden" />
-            with iPS DAO
-            </h1>
-            <p className="text-[#3F3F46] text-[16px]">
-            Empowering regenerative medicine, <br className="md:hidden" />environmental protection, <br />
-            and personalized medicine through EXO DAO and EXO tokens
-            </p>
-            <div className="fade-up flex max-md:justify-center">
-            <a href="/" className="fade-up flex items-center justify-center md:px-[22px] px-3 md:py-[14px] py-2 gap-2 rounded-xl bg-[#D9748F] text-white md:text-[18px] text-[14px] font-semibold">
-                <span><img className="max-md:w-5 md:w-6" src="/assets/images/icon-download.svg" alt="" /></span>
-                <span>White Paper</span>
-            </a>
+        <div className="px-2 md:px-20 lg:px-[240px] w-full max-md:py-10 relative flex">
+          <div className="px-3 py-5 md:p-10 bg-[#EEEDED]/[0.8]">
+            <div className="w-full max-md:max-w-[350px] md:w-[496px] md:space-y-6 space-y-4 max-md:mx-auto">
+              <h1 className="u-text-gradient bg-[linear-gradient(90deg,_#D9748F_0%,#0059BA_100%)] text-[30px] md:text-[48px] font-bold leading-[1.2] md:leading-none">
+              Join the <br className="max-md:hidden" />
+              Future of <br className="md:hidden" />Medicine <br className="max-md:hidden" />
+              with iPS DAO
+              </h1>
+              <p className="text-[#3F3F46] text-[16px]">
+              Empowering regenerative medicine, <br className="md:hidden" />environmental protection, <br />
+              and personalized medicine through EXO DAO and EXO tokens
+              </p>
+              <div className="fade-up flex max-md:justify-center">
+              <a href="/" className="fade-up flex items-center justify-center md:px-[22px] px-3 md:py-[14px] py-2 gap-2 rounded-xl bg-[#D9748F] text-white md:text-[18px] text-[14px] font-semibold">
+                  <span><img className="max-md:w-5 md:w-6" src="/assets/images/icon-download.svg" alt="" /></span>
+                  <span>White Paper</span>
+              </a>
+              </div>
             </div>
           </div>
         </div>
