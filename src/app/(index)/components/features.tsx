81327d708996d402b06ee1efcb9117840d927548
rabi-minhnhut <nhut@rabi.co.jp>
2024-10-02 19:56:04 +0700

    a

--- src/app/(index)/components/features.tsx
+++ src/app/(index)/components/features.tsx
@@ -7,13 +7,13 @@ const Features = () => {
   return (
     <section ref={ref} className="mt-8 bg-[linear-gradient(160deg,_#32BCFE_7.72%,_#2AB1FF_20.66%,_#28A7FE_31.36%,_#2594F7_45.44%,_#2379E7_64.01%)] md:[clip-path:polygon(0_0,_100%_130px,_100%_100%,_0%_100%)] [clip-path:polygon(0_0,_100%_70px,_100%_100%,_0%_100%)] px-5 overflow-hidden">
         <div className="w-full max-w-[1080px] mx-auto pt-[225px] md:pb-[180px] pb-[100px]">
-            <h3 className="fade-up text-center text-white md:text-[58px] text-[32px] font-black tracking-[0.22em]">
+            <h3 className="fade-up relative z-10 text-center text-white md:text-[58px] text-[32px] font-black tracking-[0.22em]">
             PURPOSE AND FEATURES
             </h3>
             <div className="mt-[82px] space-y-[60px]">
                 <div className="fade-up relative rounded-[20px] border border-[#2379E7] bg-white/20 [box-shadow:0px_0px_40px_0px_rgba(38,_64,_141,_0.10)] pt-[96px] pb-[112px] px-5">
                     <div className="absolute top-[-70px] md:top-[-280px] md:left-[-140px] left-[-40px] z-[-1]">
-                        <img className="max-md:w-[160px]" src="/assets/images/feature-img-01.png" alt="" />
+                        <img className="animate-[fuwafuwa_3s_ease-in-out_infinite_alternate] max-md:w-[160px]" src="/assets/images/feature-img-01.png" alt="" />
                     </div>
                     <div className="w-full max-w-[900px] mx-auto relative">
                         <h5 className="text-center md:text-[24px] text-[18px] font-black tracking-widest pb-10 border-b border-white"> Purpose of DwETH</h5>
@@ -24,10 +24,10 @@ const Features = () => {
                 </div>
                 <div className="fade-up relative rounded-[20px] border border-[#2379E7] bg-white/20 [box-shadow:0px_0px_40px_0px_rgba(38,_64,_141,_0.10)] pt-[100px] pb-[90px] px-5">
                     <div className="absolute md:top-[-150px] top-[-50px] md:right-[-170px] right-[-60px] z-[-1]">
-                        <img className="fade-up max-md:w-[160px]" src="/assets/images/feature-img-02.png" alt="" />
+                        <img className="fade-up animate-[fuwafuwa_3s_ease-in-out_infinite_alternate] max-md:w-[160px]" src="/assets/images/feature-img-02.png" alt="" />
                     </div>
                     <div className="absolute bottom-[-60px] md:bottom-[-120px] md:left-[-180px] left-[-40px] z-[-1]">
-                        <img className="fade-up max-md:w-[140px]" src="/assets/images/feature-img-03.png" alt="" />
+                        <img className="fade-up animate-[fuwafuwa_3s_ease-in-out_infinite_alternate] max-md:w-[140px]" src="/assets/images/feature-img-03.png" alt="" />
                     </div>
                     <div className="w-full max-w-[900px] mx-auto relative">
                         <h5 className="text-center md:text-[24px] text-[18px] font-black tracking-widest pb-10 border-b border-white"> Key Features of DwETH</h5>
--- src/app/(index)/components/fv.tsx
+++ src/app/(index)/components/fv.tsx
@@ -2,15 +2,14 @@
 import useScrollOut from "@/hooks/useScrollOut";
 
 const FV = () => {
-  useScrollOut('.fv-scroll', {
+  useScrollOut(".fv-scroll", {
     offset: 50,
     cssProps: true,
   });
   return (
     <div>
-      <div className="fv-scroll group pt-[100px] h-[760px] relative">
-      <div className="absolute top-0 inset-x-0 h-full bg-[linear-gradient(140deg,_#2379E7_1.59%,_#2594F7_33.42%,_#28A7FE_57.52%,_#2AB1FF_75.85%,_#32BCFE_98.03%)] md:[clip-path:polygon(0_0,_100%_0,_100%_calc(100%-140px),_0%_100%)] [clip-path:polygon(0_0,_100%_0,_100%_calc(100%-70px),_0%_100%)]">
-      </div>
+      <div className="fv-scroll group pt-[100px] h-screen relative">
+        <div className="absolute top-0 inset-x-0 h-[80%] bg-[linear-gradient(140deg,_#2379E7_1.59%,_#2594F7_33.42%,_#28A7FE_57.52%,_#2AB1FF_75.85%,_#32BCFE_98.03%)] md:[clip-path:polygon(0_0,_100%_0,_100%_calc(100%-140px),_0%_100%)] [clip-path:polygon(0_0,_100%_0,_100%_calc(100%-70px),_0%_100%)]"></div>
         <div className="fixed top-0 inset-x-0 h-full w-full max-w-[1440px] mx-auto flex items-center justify-center z-0">
           <div className="group-data-[scroll=in]:translate-x-[100%] group-data-[scroll=in]:translate-y-[40%] group-data-[scroll=in]:rotate-[-30deg] group-data-[scroll=in]:opacity-0 duration-300 absolute w-[24.375%] top-[25%] md:top-[12%] md:left-[2.083%] left-[12%]">
             <img src="/assets/images/fc-sec-01.png" alt="" />
@@ -30,26 +29,56 @@ const FV = () => {
           <div className="group-data-[scroll=in]:translate-x-[-120%] group-data-[scroll=in]:translate-y-[-20%] group-data-[scroll=in]:rotate-[20deg] group-data-[scroll=in]:opacity-0 duration-300 absolute w-[17.08%] right-[11%] top-[60%] md:top-[58%]">
             <img src="/assets/images/fc-sec-06.png" alt="" />
           </div>
-          <div className="bg-white font-bold text-[22px] md:text-[48px] text-[#1E1E1E] px-5 md:px-[50px] py-2.5 md:max-w-[750px] max-w-[350px] relative" style={{ filter: 'blur(calc((1 - var(--visible-y)) * 10px))', opacity: 'var(--visible-y)' }}>
-              Unwavering Commitment to Transparency and Security
+          <div
+            className="bg-white font-bold text-[15px] md:text-[20px] text-[#1E1E1E] px-4 md:px-[32px] py-5 md:max-w-[750px] max-w-[450px] relative"
+            style={{
+              filter: "blur(calc((1 - var(--visible-y)) * 10px))",
+              opacity: "var(--visible-y)",
+            }}
+          >
+            <span className="md:text-[28px] text-[18px]">DwETH: Ethereum Reborn on DIVER Chain </span><br />
+            ・1 DwETH = 1 ETH Guaranteed Value<br />
+            ・Smooth Transactions with Zero Fees<br />
+            ・Lightning-Fast 2-Second Processing<br />
+            ・ETH⇄DwETH Easy Bridges<br />
+            ・Unwavering Commitment to Transparency and Security
           </div>
-          </div>
-          </div>
-        <div className="flex justify-center items-center flex-col px-5">
           
-        <p className="text-[28px] md:text-[82px] text-center font-black text-[#1E1E1E]">
-            EthereWum, Evolving <br />Further with DIVER
-            </p>
-          <a href="/" className="w-[300px] md:w-[604px] relative aspect-square bg-[linear-gradient(140deg,_#040444_15.67%,_#0D1054_38.25%,_#101764_55.35%,_#131D74_68.35%,_#15206B_84.08%)] [box-shadow:-4px_4px_180px_0px_rgba(85,_194,_255,_0.50),_0px_4px_84px_0px_rgba(0,_0,_0,_0.25)] rounded-full flex justify-center items-center">
-            <div className="absolute inset-0 flex items-center justify-center flex-col gap-2">
-              <div className="h-8"></div>
-              <figure className="[filter:drop-shadow(0px_20px_53px_rgba(50,_189,_254,_0.40))_drop-shadow(0px_4px_15px_rgba(0,_0,_0,_0.25))]"><img className="max-md:w-[100px]" src="/assets/images/logo-white.svg" alt="" /></figure>
-              <p className="text-center h-8 text-[16px] md:text-[20px] text-white font-semibold tracking-[6px]">Download</p>
-              <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-[36px] md:text-[58px] font-black text-white tracking-widest">
-              WHITE <br />PAPER</div>
-            </div>
-            </a>
+          
+      <p className="absolute bottom-0 text-[28px] lg:text-[82px] md:text-[44px] text-center font-black leading-[1.3] text-[#1E1E1E] [text-shadow:3px_3px_3px_#fff]" style={{
+              filter: "blur(calc((1 - var(--visible-y)) * 10px))",
+              opacity: "var(--visible-y)",
+            }}>
+          Ethereum, Evolving <br />
+          Further with DIVER
+        </p>
         </div>
+      </div>
+      
+      <div className="flex justify-center items-center flex-col px-5 md:mt-[100px]">
+        <a
+          href="/"
+          className="w-[300px] md:w-[604px] relative aspect-square bg-[linear-gradient(140deg,_#040444_15.67%,_#0D1054_38.25%,_#101764_55.35%,_#131D74_68.35%,_#15206B_84.08%)] [box-shadow:-4px_4px_180px_0px_rgba(85,_194,_255,_0.50),_0px_4px_84px_0px_rgba(0,_0,_0,_0.25)] rounded-full flex justify-center items-center"
+        >
+          <div className="absolute inset-0 flex items-center justify-center flex-col gap-2">
+            <div className="h-8"></div>
+            <figure className="[filter:drop-shadow(0px_20px_53px_rgba(50,_189,_254,_0.40))_drop-shadow(0px_4px_15px_rgba(0,_0,_0,_0.25))]">
+              <img
+                className="max-md:w-[100px]"
+                src="/assets/images/logo-white.svg"
+                alt=""
+              />
+            </figure>
+            <p className="text-center h-8 text-[16px] md:text-[20px] text-white font-semibold tracking-[6px]">
+              Download
+            </p>
+            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-[36px] md:text-[58px] font-black text-white tracking-widest">
+              WHITE <br />
+              PAPER
+            </div>
+          </div>
+        </a>
+      </div>
     </div>
   );
 };
