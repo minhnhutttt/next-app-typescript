2d7794fec19a75480a9def91249f5c3489bed1c3
rabi-minhnhut <nhut@rabi.co.jp>
2024-10-16 18:09:21 +0700

    z

--- src/components/underlineText.tsx
+++ src/components/underlineText.tsx
@@ -5,40 +5,8 @@ const UnderlineText = ({ children }: { children: ReactNode }) => {
   return (
     <span data-scroll className="group relative">
       {children}
-      <span className="w-full block md:h-4 h-3 absolute top-auto md:-bottom-[12px] -bottom-[7px] left-[.0625rem] right-auto">
-        <span className="justify-center items-center w-full h-full flex">
-          <svg
-            className="w-full h-full"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-            className="group-data-[scroll=in]:animate-[underlineText_1s_ease-in-out_forwards]"
-              id="control-line"
-              pathLength="1"
-              d="M3 13C49.7892 6.25733 96.9501 3 144 3"
-              stroke="url(#paint0_linear_463_797)"
-              strokeWidth="5"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeDashoffset={1}
-              strokeDasharray={1}
-            ></path>
-            <defs>
-              <linearGradient
-                id="paint0_linear_463_797"
-                x1="3"
-                y1="8"
-                x2="144"
-                y2="8"
-                gradientUnits="userSpaceOnUse"
-              >
-                <stop stopColor="#FDB83F"></stop>
-                <stop offset="1" stopColor="#950AF7"></stop>
-              </linearGradient>
-            </defs>
-          </svg>
-        </span>
+      <span className="w-full block md:h-4 h-3 absolute top-auto md:-bottom-[12px] -bottom-[7px] left-[.0625rem] right-auto bg-no-repeat bg-right bg-[url('/assets/images/underline.svg')]">
+       
       </span>
     </span>
   );
