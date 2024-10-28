306cc9a63ed3b90d01d48299b24ca2457cd8f275
rabi-minhnhut <nhut@rabi.co.jp>
2024-10-28 14:04:45 +0700

    a

---
+++ src/app/(index)/components/fv.tsx
@@ -0,0 +1,11 @@
+"use client";
+import useScrollAnimations from "@/hooks/useScrollAnimations";
+
+const FV = () => {
+  const ref = useScrollAnimations();
+  return (
+    <div></div>
+  );
+};
+
+export default FV;
