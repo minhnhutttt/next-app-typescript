7251a5b9c79ff1c6e9566f9072b6d9f1baa14adf
rabi-minhnhut <nhut@rabi.co.jp>
2024-12-03 21:31:08 +0700

    a

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -6,7 +6,7 @@ const Button = () => {
       href="/whitepaper"
       className=" flex items-center justify-end gap-4 tracking-[0.2em] duration-150 hover:opacity-70 md:text-[20px]"
     >
-      <span>WHITEPEPAR</span>
+      <span>WHITEPAPER</span>
       <span>
         <img
           className="relative rounded-full [box-shadow:0px_8px_30px_0px_rgba(134,1,0,0.4)] max-md:w-[60px]"
