9d312c464749a8d53492e56fd03815fccdec2ecd
rabi-minhnhut <nhut@rabi.co.jp>
2024-12-10 18:04:58 +0700

    a

--- src/components/waterText.tsx
+++ src/components/waterText.tsx
@@ -1,88 +1,33 @@
-import React, { useState, useRef, useEffect } from 'react';
 //@ts-ignore
-import { LiquidDistortionText, DistortionText } from 'react-text-fun';
+import { DistortionText } from "react-text-fun";
 
 export default function WaterText() {
-  const [speed, setSpeed] = useState(0.45);
-  const targetSpeed = useRef(0.45);
-  const animationFrameId = useRef<number | null>(null);
-
-  const smoothSpeedChange = () => {
-    if (animationFrameId.current) {
-      cancelAnimationFrame(animationFrameId.current);
-    }
-
-    const updateSpeed = () => {
-      setSpeed((currentSpeed) => {
-        const diff = targetSpeed.current - currentSpeed;
-        const step = 0.001;
-
-        if (Math.abs(diff) < step) {
-          return targetSpeed.current;
-        }
-
-        return currentSpeed + Math.sign(diff) * step;
-      });
-
-      if (speed !== targetSpeed.current) {
-        animationFrameId.current = requestAnimationFrame(updateSpeed);
-      }
-    };
-
-    updateSpeed();
-  };
-
-  const handleMouseMove = () => {
-    targetSpeed.current = 0.6;
-    smoothSpeedChange();
-  };
-
-  const handleMouseLeave = () => {
-    targetSpeed.current = 0.45;
-    smoothSpeedChange();
-  };
-
-  useEffect(() => {
-    return () => {
-      if (animationFrameId.current) {
-        cancelAnimationFrame(animationFrameId.current);
-      }
-    };
-  }, []);
-
   return (
-    <div className="flex justify-center">
-    <div
-      onMouseMove={handleMouseMove}
-      onMouseLeave={handleMouseLeave}
-      style={{ display: 'inline-block' }}
-    >
-        
+    <div className="flex justify-center *:flex *:justify-center *:flex-col *:items-center">
       <DistortionText
-  text="　　DIVER　　"
-  fill="white"
+        text="　　DIVER　　"
+        fill="white"
         fontFamily="League Spartan"
         fontSize={120}
-        speed={speed}
+        speed={0.4}
         rotation={45}
         distortX={0.5}
         distortY={0.5}
         noiseAmplitude={0.12}
         noiseVolatility={1}
-/>
-<DistortionText
-  text="　　Time Network　　"
-  fill="white"
+      />
+      <DistortionText
+        text="　　Time Network　　"
+        fill="white"
         fontFamily="League Spartan"
-  fontSize={120}
-  speed={speed}
-  rotation={45}
-  distortX={0.5}
-  distortY={0.5}
-  noiseAmplitude={0.12}
-  noiseVolatility={1}
-/>
-    </div>
+        fontSize={120}
+        speed={0.4}
+        rotation={45}
+        distortX={0.5}
+        distortY={0.5}
+        noiseAmplitude={0.12}
+        noiseVolatility={1}
+      />
     </div>
   );
 }
