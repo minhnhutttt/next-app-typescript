110ef9cc0e1657a5386caa6d3a54bc0e1a065c23
rabi-minhnhut <nhut@rabi.co.jp>
2025-01-01 00:40:27 +0700

    a

--- src/app/(index)/components/faq/faqItem.tsx
+++ src/app/(index)/components/faq/faqItem.tsx
@@ -1,6 +1,11 @@
 "use client";
 import { ReactNode, useRef, useState } from "react";
 import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
+gsap.config({
+  nullTargetWarn: false,
+});
 
 export type FaqItemPropsType = {
   id: string;
@@ -9,19 +14,20 @@ export type FaqItemPropsType = {
 };
 
 export default function FaqItem({ id, question, answer }: FaqItemPropsType) {
-  const contentRef = useRef(null);
-  const arrowRef = useRef(null);
+  const contentRef = useRef<HTMLDivElement>(null);
   const isClosedRef = useRef(true);
   const [isClose, setIsClose] = useState(true);
 
   const handleExpanderClick = () => {
     const $content = contentRef.current;
+    if (!$content) return;
+
     if (isClosedRef.current) {
       gsap.set($content, { height: "auto" });
-      gsap.from($content, { duration: 0.2, height: 0 });
+      gsap.from($content, { duration: 0.2, height: 0, onComplete: ScrollTrigger.refresh });
       setIsClose(false);
     } else {
-      gsap.to($content, { duration: 0.2, height: 0 });
+      gsap.to($content, { duration: 0.2, height: 0, onComplete: ScrollTrigger.refresh });
       setIsClose(true);
     }
     isClosedRef.current = !isClosedRef.current;
@@ -40,22 +46,15 @@ export default function FaqItem({ id, question, answer }: FaqItemPropsType) {
           </p>
         </div>
         <div className="flex justify-between">
-        <p className="text-left text-[20px] font-bold md:text-[32px]">
-            {question}
-          </p>
-          <div
-          className="relative ml-2 flex size-8 items-center justify-center duration-150 md:flex-[0_0_51px] flex-[0_0_32px] md:size-[51px]"
-        >
-            <img className={`${isClose && 'opacity-100'}`} src="/assets/images/ic-faq-close.svg" alt="" />
-            <img className={`absolute inset-0 ${isClose && 'opacity-0'}`} src="/assets/images/ic-faq-open.svg" alt="" />
+          <p className="text-left text-[20px] font-bold md:text-[32px]">{question}</p>
+          <div className="relative ml-2 flex size-8 items-center justify-center duration-150 md:flex-[0_0_51px] flex-[0_0_32px] md:size-[51px]">
+            <img className={`${isClose && "opacity-100"}`} src="/assets/images/ic-faq-close.svg" alt="" />
+            <img className={`absolute inset-0 ${isClose && "opacity-0"}`} src="/assets/images/ic-faq-open.svg" alt="" />
+          </div>
         </div>
-        </div>
-        
       </button>
       <div ref={contentRef} className="h-0 overflow-hidden">
-        <p className="md:text-[28px] text-[20px] leading-[1.4] md:pt-9 pt-5">
-          {answer}
-        </p>
+        <p className="md:text-[28px] text-[20px] leading-[1.4] md:pt-9 pt-5">{answer}</p>
       </div>
     </div>
   );
