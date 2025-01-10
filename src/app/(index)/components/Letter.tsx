64049c5dc17aca1f6bb37ee2b6b80b1b6562ac15
rabi-minhnhut <nhut@rabi.co.jp>
2025-01-10 22:24:07 +0700

    a

--- src/app/(index)/components/Letter.tsx
+++ src/app/(index)/components/Letter.tsx
@@ -12,7 +12,7 @@ const Letter = () => {
     >
       <div className="w-full max-w-[1000px] mx-auto flex">
        <div className="fade-up w-[417px] p-px u-gradient flex rounded-[30px]">
-        <div className="bg-white bg-[url(/assets/images/wing.png)] bg-no-repeat bg-center bg-contain w-full h-full rounded-[30px] px-7 md:py-[56px] py-8 md:px-11 md:text-[20px] text-[16px] leading-loose tracking-wider">
+        <div className="bg-white bg-[url(/assets/images/wing.png)] bg-no-repeat bg-center bg-[size:276px_auto] w-full h-full rounded-[30px] px-7 md:py-[56px] py-8 md:px-11 md:text-[20px] text-[16px] leading-loose tracking-wider">
         BYT Coin goes beyond a simple point token and aims to become a platform for chain value exchange starting from beauty experiences. Like the chain of connections that the word blockchain represents, it seamlessly connects various aesthetic experiences and value exchange, contributing to the realization of a richer lifestyle. <br />
         Through this innovative platform, we will provide new value not only to the beauty industry but to all related industries and users.
         </div>
