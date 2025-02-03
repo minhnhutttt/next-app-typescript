11ba48f9bdc108ae40e2facbf8bd485df3aa8d89
rabi-minhnhut <nhut@rabi.co.jp>
2025-02-03 22:44:50 +0700

    a

---
+++ src/components/modal.tsx
@@ -0,0 +1,36 @@
+"use client"
+import { useEffect } from "react";
+
+interface ModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  children: React.ReactNode;
+}
+
+const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children }) => {
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "";
+    }
+  }, [isOpen]);
+
+  if (!isOpen) return null;
+
+  return (
+    <div
+      className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50"
+      onClick={onClose}
+    >
+      <div
+        className="relative w-full md:max-w-[500px] max-w-[300px]"
+        onClick={(e) => e.stopPropagation()}
+      >
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
