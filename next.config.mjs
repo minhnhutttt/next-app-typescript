6d92901d448d175cfca1b09c4b8c58725a92e2ca
rabi-minhnhut <nhut@rabi.co.jp>
2025-02-07 20:14:01 +0700

    a

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
+  reactStrictMode: true,
+  output: 'export',
+  trailingSlash: true,
   };
   
   export default nextConfig;
