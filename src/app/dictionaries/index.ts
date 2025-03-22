271c8a7fb7219863ef9715943213baf9eb011cb8
rabi-minhnhut <nhut@rabi.co.jp>
2025-03-22 21:32:10 +0700

    s

--- src/app/dictionaries/index.ts
+++ src/app/dictionaries/index.ts
@@ -1,17 +1,13 @@
-// dictionaries/index.ts
 import { Dictionary } from './types';
 
-// Định nghĩa các locale được hỗ trợ dưới dạng union type
 export type Locale = 'en' | 'ja' | 'zh';
 
-// Định nghĩa object dictionaries với kiểu dữ liệu rõ ràng
 const dictionaries: Record<Locale, () => Promise<Dictionary>> = {
   en: () => import('./en.json').then(module => module.default),
   ja: () => import('./ja.json').then(module => module.default),
   zh: () => import('./zh.json').then(module => module.default),
 };
 
-// getDictionary nhận vào locale và trả về Promise của Dictionary
 export const getDictionary = async (locale: Locale): Promise<Dictionary> => {
   return dictionaries[locale]();
 };
