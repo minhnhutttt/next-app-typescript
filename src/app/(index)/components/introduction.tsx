8c099a7d088e6bc8112700ac42778135177b1a68
rabi-minhnhut <nhut@rabi.co.jp>
2025-05-08 20:00:45 +0700

    a

--- src/app/(index)/components/introduction.tsx
+++ src/app/(index)/components/introduction.tsx
@@ -1,12 +1,40 @@
 'use client'
-
+import { gsap } from "gsap";
 import LineButton from '@/components/lineButton'
 import Title from '@/components/title'
 import useScrollAnimations from '@/hooks/useScrollAnimations'
+import { useRef } from 'react'
 
 const Introduction = () => {
   const ref = useScrollAnimations()
-
+  const contentRef = useRef(null);
+  const titleRef = useRef(null);
+  const arrowRef = useRef(null);
+  const triangleRef = useRef(null);
+  const isClosedRef = useRef(true);
+  const handleExpanderClick = () => {
+    const $content = contentRef.current;
+    const $arrow = arrowRef.current;
+    const $title = titleRef.current;
+    const $triangle = triangleRef.current;
+    if (isClosedRef.current) {
+      gsap.set($content, { height: "auto" });
+      gsap.from($content, { duration: 0.2, height: 0 });
+      gsap.to($title, { duration: 0.2, yPercent: 100 });
+      gsap.to($triangle, { duration: 0.2, yPercent: 100, opacity: 1 });
+      gsap.to($arrow, { duration: 0.1, rotation: 0 });
+    } else {
+      gsap.to($content, { duration: 0.2, height: 0 });
+      gsap.to($title, { duration: 0.2, yPercent: 0 });
+      gsap.to($triangle, { duration: 0.2, yPercent: 0, opacity: 0 });
+      gsap.to($arrow, {
+        duration: 0.1,
+        rotation: -90,
+        transformOrigin: "center",
+      });
+    }
+    isClosedRef.current = !isClosedRef.current;
+  };
   return (
     <section ref={ref} className="px-5 pt-10 md:pt-[52px]">
       <div className="fade-up flex justify-center">
@@ -27,6 +55,83 @@ const Introduction = () => {
             </div>
           </div>
         </a>
+      </div>
+      <div className="md:pt-[60px] pt-10">
+        <div className="h-[5px] bg-[linear-gradient(90deg,_#FF7DD3_0%,_#3CE8FF_100%)]"></div>
+        <div className="md:pt-[76px] pt-[50px]">
+          <div className="fade-up relative mx-auto flex w-full max-w-[414px] pb-[36px]">
+            <div className="absolute md:top-[-75px] top-[-55px] left-0">
+              <img
+                className="max-md:w-[90px]"
+                src="/assets/images/crown.svg"
+                alt=""
+              />
+            </div>
+            <h4>
+              <img src="/assets/images/top-1-title.svg" alt="全日本1位を獲得" />
+            </h4>
+          </div>
+        </div>
+        <div>
+          <video autoPlay muted loop playsInline preload="auto" className="">
+            <source
+              src="/assets/videos/Goodfellows_17LIVE_no1.mp4"
+              type="video/mp4"
+            />
+            Your browser does not support the video tag.
+          </video>
+        </div>
+        <div className="md:py-[47px] py-8">
+          <button
+            className="flex justify-between gap-5 w-full border border-[#D2953F] bg-[url(/assets/images/bg-top.png)] bg-cover bg-no-repeat relative"
+            onClick={handleExpanderClick}
+          >
+            <div className="overflow-hidden flex-1">
+            <h5 ref={titleRef} className="text-left md:text-[18px] text-[16px] font-bold leading-[1.3] p-2">
+              このたび開催されました、『戦国時代天下布武!!
+              エージェンシー合戦!!!』 において、弊社は
+              全国第１位 という栄誉ある賞を賜りました。
+            </h5>
+            </div>
+            <div className="bg-[#FFC9C9] flex justify-center items-center md:w-20 w-12">
+              <span className="w-4 h-0.5 bg-black"></span>
+              <span
+                ref={arrowRef}
+                className="w-4 h-0.5 bg-black absolute rotate-90"
+              ></span>
+            </div>
+            <span ref={triangleRef} className="absolute opacity-0 -translate-y-full top-full left-10 aspect-[3/2] w-[30px] bg-[url(/assets/images/bg-top-arrow.png)] bg-cover"></span>
+          </button>
+          <div ref={contentRef} className="overflow-hidden h-0 bg-[url(/assets/images/bg-cup.png)] bg-cover bg-no-repeat text-[16px] md:text-[20px] font-medium">
+          <p className="px-3 py-10">
+            このたび開催されました、
+            <br />
+            『戦国時代天下布武!! エージェンシー合戦!!!』 において、弊社は
+            全国第１位 という栄誉ある賞を賜りました。
+            <br />
+            <br />
+            この快挙は、日々配信に全力で取り組んでくださるライバーの皆さま、
+            <br />
+            温かい応援をお寄せくださるリスナーの皆さま、
+            <br />
+            そして 17LIVE を陰で支えてくださる運営スタッフの皆さま
+            のご支援の賜物にほかなりません。
+            <br />
+            <br />
+            心より御礼申し上げます。
+            <br />
+            <br />
+            今後とも、微力ながら17LIVE及びライブ配信業界の発展に寄与すべく精進してまいる所存でございます。
+            <br />
+            <br />
+            引き続き、皆さまのご指導ご鞭撻とご支援を賜りますよう、何卒よろしくお願い申し上げます。
+            <br />
+            <br />
+            皆さま本当にありがとうございました！！
+            </p>
+          </div>
+        </div>
+        <div className="h-[5px] bg-[linear-gradient(90deg,_#FF7DD3_0%,_#3CE8FF_100%)]"></div>
       </div>
       <div className="mt-8 md:hidden">
         <LineButton />
